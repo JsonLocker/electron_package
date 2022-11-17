08f0e343626591aad08702d372d5cb3e82b68ca1
Czzor <Czzor@gmail.com>
2022-11-17 13:46:28 +0800

    开始自动更新逻辑

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 // Modules to control application life and create native browser window
+const {autoUpdater} = require('electron-updater')
 const {app, BrowserWindow,Menu} = require('electron')
 const path = require('path')
 
@@ -11,6 +12,7 @@ app.setLoginItemSettings({
 function createWindow () {
 
 	const url = "https://haomooc.com/index.php?c=page&id=15";
+	const updateUrl = "https://haomooc.com/uploads/desktop_app/yinong";
 	const appName = "益农直通车";
 
 	// Create the browser window.
@@ -73,3 +75,48 @@ app.on('window-all-closed', function () {
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
+
+// 检测更新，在你想要检查更新的时候执行，renderer事件触发后的操作自行编写
+function updateHandle(){
+    let message={
+      error:'检查更新出错',
+      checking:'正在检查更新……',
+      updateAva:'检测到新版本，正在下载……',
+      updateNotAva:'现在使用的就是最新版本，不用更新',
+    };
+    const os = require('os');
+    autoUpdater.setFeedURL(updateUrl);
+    autoUpdater.on('error', function(error){
+      sendUpdateMessage(message.error)
+    });
+    autoUpdater.on('checking-for-update', function() {
+      sendUpdateMessage(message.checking)
+    });
+    autoUpdater.on('update-available', function(info) {
+        sendUpdateMessage(message.updateAva)
+    });
+    autoUpdater.on('update-not-available', function(info) {
+        sendUpdateMessage(message.updateNotAva)
+    });
+    
+    // 更新下载进度事件
+    autoUpdater.on('download-progress', function(progressObj) {
+        mainWindow.webContents.send('downloadProgress', progressObj)
+    })
+    autoUpdater.on('update-downloaded',  function (event, releaseNotes, releaseName, releaseDate, updateUrl, quitAndUpdate) {
+        ipcMain.on('isUpdateNow', (e, arg) => {
+            //some code here to handle event
+            autoUpdater.quitAndInstall();
+        })
+        mainWindow.webContents.send('isUpdateNow')
+    });
+    
+    //执行自动更新检查
+    autoUpdater.checkForUpdates();
+}
+
+// 通过main进程发送事件给renderer进程，提示更新信息
+// mainWindow = new BrowserWindow()
+function sendUpdateMessage(text){
+    mainWindow.webContents.send('message', text)
+}
